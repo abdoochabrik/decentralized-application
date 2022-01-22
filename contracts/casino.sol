01675d3428444ffe9085430cda065f031e25cf06
Dell <abdoo.chbrik@gmail.com>
2022-01-22 17:13:42 +0100

    distributePrizes function to Send the corresponding ether to each winner depending on the total bets

--- contracts/casino.sol
+++ contracts/casino.sol
@@ -44,4 +44,23 @@ contract Casino {
       uint256 numberGenerated = block.number % 10 + 1; // This isn't secure
       distributePrizes(numberGenerated);
    }
+   // Sends the corresponding ether to each winner depending on the total bets
+   function distributePrizes(uint256 numberWinner) public {
+      address[100] memory winners; // We have to create a temporary in memory array with fixed size
+      uint256 count = 0; // This is the count for the array of winners
+      for(uint256 i = 0; i < players.length; i++){
+         address playerAddress = players[i];
+         if(playerInfo[playerAddress].numberSelected == numberWinner){
+            winners[count] = playerAddress;
+            count++;
+         }
+         delete playerInfo[playerAddress]; // Delete all the players
+      }
+      players.length = 0; // Delete all the players array
+      uint256 winnerEtherAmount = totalBet / winners.length; // How much each winner gets
+      for(uint256 j = 0; j < count; j++){
+         if(winners[j] != address(0)) // Check that the address in this fixed array is not empty
+         winners[j].transfer(winnerEtherAmount);
+      }
+   }
 }
